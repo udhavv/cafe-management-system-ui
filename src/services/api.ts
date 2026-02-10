08899d91fd4a36c98364a00c3c78f97daa5c6290
udhavv <auddhav074@gmail.com>
2026-02-10 18:00:48 +0545

    adding api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
-    baseURL: "http://localhost:4000",
+    // baseURL: "http://localhost:4000",
+    baseURL: "https://cafe-management-system-b9fa.onrender.com",
 
   withCredentials: true, // ✅ SEND/RECEIVE COOKIES
   headers: { "Content-Type": "application/json" },
